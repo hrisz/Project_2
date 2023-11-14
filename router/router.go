c3698e304fd9a275e186bf4ba3d1a3f402d58140
hrisz <harissaefuloh@gmail.com>
2023-11-14 11:19:15 +0700

    add router

---
+++ router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	routeNaskah "project_2/internal/routes/routenaskah"
+	routePenulis "project_2/internal/routes/routepenulis"
+	
+)
+
+func SetupRoutes(app *fiber.App) {
+	api := app.Group("/api", logger.New())
+	routeNaskah.SetupDataNaskahRoutes(api)
+	routePenulis.SetupDataPenulisRoutes(api)
+}
