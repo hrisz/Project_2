b059f45db2e74fd6f95879e3a65ba96f14d95dd9
hrisz <harissaefuloh@gmail.com>
2023-12-14 02:28:06 +0700

    fix frontend

--- src/js/transisi.js
+++ src/js/transisi.js
@@ -1,12 +1,12 @@
 function toggleSections(section) {
-    // Get references to the dashboard and profile sections
+    
     const dashboardSection = document.getElementById('dashboardSection');
-    const profileSection = document.getElementById('profileSection');
-
-    // Toggle the 'hidden' class based on the clicked button
+    const profileSection = document.getElementById('profilSection');
+    
     if (section === 'dashboard') {
         dashboardSection.style.display = 'block';
         profileSection.style.display = 'none';
+
 
     } else if (section === 'profil') {
         dashboardSection.style.display = 'none';
