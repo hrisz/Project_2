36520bf4f60c7117f2f2d6df585cf5e72f5f9538
hrisz <harissaefuloh@gmail.com>
2023-12-14 10:34:37 +0700

    add databook table menu

--- src/js/transisi.js
+++ src/js/transisi.js
@@ -1,16 +1,24 @@
 function toggleSections(section) {
     
     const dashboardSection = document.getElementById('dashboardSection');
-    const profileSection = document.getElementById('profilSection');
+    const profileSection = document.getElementById('profileSection');
+    const bookSection = document.getElementById('bookSection')
     
     if (section === 'dashboard') {
         dashboardSection.style.display = 'block';
         profileSection.style.display = 'none';
-
-
-    } else if (section === 'profil') {
+        bookSection.style.display = 'none';
+    }
+    
+    else if (section === 'profile') {
         dashboardSection.style.display = 'none';
         profileSection.style.display = 'block';
-        
+        bookSection.style.display = 'none';
+    }
+    
+    else if (section === 'book') {
+        dashboardSection.style.display = 'none';
+        profileSection.style.display = 'none';
+        bookSection.style.display = 'block';
     }
 }
